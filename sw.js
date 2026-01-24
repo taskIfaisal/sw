80e450137d50bf42f546a268275071506654cb6c
faisal.io <50693350+taskIfaisal@users.noreply.github.com>
2026-01-24 09:51:49 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'magic-card-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './icon/slide-512x512.png',
+  './icon/slide-192x192.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch events
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
